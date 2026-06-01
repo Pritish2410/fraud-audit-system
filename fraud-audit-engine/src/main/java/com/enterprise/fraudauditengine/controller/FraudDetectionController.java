fafa833a3399138fa9b9ba18d2438a4df0247172
Pritish Kumar Sahoo <pritishsahoo24october2003@gmail.com>
2026-06-02 03:09:55 +0530

    Added CORS configuration to allow Vercel frontend

--- fraud-audit-engine/src/main/java/com/enterprise/fraudauditengine/controller/FraudDetectionController.java
+++ fraud-audit-engine/src/main/java/com/enterprise/fraudauditengine/controller/FraudDetectionController.java
@@ -3,31 +3,28 @@ package com.enterprise.fraudauditengine.controller;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.http.*;
 import com.enterprise.fraudauditengine.redis.FraudProducer;
+
 /**
  * REST Controller exposing the Redis Event Stream to external clients.
  */
 @RestController
 @RequestMapping("/api/v1/audit")
-@CrossOrigin(origins = "http://localhost:5173") // Crucial: Allows your Vite React app to connect
+@CrossOrigin(origins = "*") // The VIP list is now open to the public internet!
 public class FraudDetectionController {
 
     private final FraudProducer fraudProducer;
 
-    // Pro-standard constructor injection
     public FraudDetectionController(FraudProducer fraudProducer) {
         this.fraudProducer = fraudProducer;
     }
-
     
     @PostMapping("/trigger")
     public ResponseEntity<String> triggerFraudProtocol() {
         try {
-            // Instantly push the event to Redis and return success to the UI
             fraudProducer.streamBlockedAccount("WAYNE_ENT_001");
             return ResponseEntity.ok("ACCOUNT_ISOLATED");
             
         } catch (Exception e) {
-            // Enterprise standard error handling
             return ResponseEntity
                     .status(HttpStatus.INTERNAL_SERVER_ERROR)
                     .body("CRITICAL ERROR: Redis stream failed to process the request. Reason: " + e.getMessage());
